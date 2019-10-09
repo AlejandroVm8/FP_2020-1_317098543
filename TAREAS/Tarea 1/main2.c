ecf2146a0adf53e2d74d210139fa300f96911716
Alejandro <alejandrovalencia.988@gmail.com>
2019-10-08 23:43:13 -0500

    Tareas

---
+++ TAREAS/Tarea 1/main2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+ int main(int argc, char const *argv[])
+{
+	char caracter  [1];
+    printf("Ingresa una palabra ");
+     scanf("%s", &caracter);
+    printf("palabra: %s", caracter);
+    
+	return 0;
+}
---
+++ TAREAS/Tarea 2/main.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+
+{
+	int x;
+	printf("Ingrese un numero: ");
+	scanf("%d",&x);
+	if (x%2==0)
+		
+			printf("0\n"); 
+	
+	else{
+	
+	
+		printf("1\n");
+} 
+	return 0;
+}
+
+
